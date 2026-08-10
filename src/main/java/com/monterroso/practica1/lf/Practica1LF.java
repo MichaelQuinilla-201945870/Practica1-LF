afd71d923a02af769868bc7031a80ac72163d035
MichaelQuinilla-201945870 <michaelquinilla201945870@cunoc.edu.gt>
2026-08-09 18:06:59 -0600

    Commit inicial.

---
+++ src/main/java/com/monterroso/practica1/lf/Practica1LF.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.monterroso.practica1.lf;
+
+/**
+ *
+ * @author seo
+ */
+public class Practica1LF {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
